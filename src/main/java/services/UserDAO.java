aaebe853260d5d5fc0000442206ac8f3738c4f8c
morev <Ilya>
2016-05-12 16:25:23 +0300

    fixing issues

--- src/main/java/services/UserDAO.java
+++ src/main/java/services/UserDAO.java
@@ -1,5 +1,7 @@
 package services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -12,6 +14,7 @@ import java.util.List;
  * Created by morev on 06.04.16.
  */
 public class UserDAO {
+    private static final Logger logger = LogManager.getLogger(AccountServiceOnHibernate.class);
     private final Session session;
 
     UserDAO(Session session) {
@@ -19,14 +22,26 @@ public class UserDAO {
     }
 
     public UserProfile getUser(long id) {
-        return session.get(UserProfile.class, id);
+        try {
+            return session.get(UserProfile.class, id);
+        } catch (Exception e) {
+            logger.error("Database failed ", e);
+            return null;
+        }
     }
 
     public UserProfile getUser(String login) {
-        Criteria criteria = session.createCriteria(UserProfile.class);
-        return (UserProfile) criteria
-                .add(Restrictions.eq("login", login))
-                .uniqueResult();
+        try {
+            Criteria criteria = session.createCriteria(UserProfile.class);
+            return (UserProfile) criteria
+                    .add(Restrictions.eq("login", login))
+                    .uniqueResult();
+        } catch (Exception e) {
+            logger.error("Database failed ", e);
+            return null;
+        }
+
+
     }
 
 
@@ -35,23 +50,41 @@ public class UserDAO {
     }
 
     public void addUser(UserProfile user) {
-        Transaction transaction = session.beginTransaction();
-        session.save(user);
-        transaction.commit();
+        try {
+            session.getTransaction().begin();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Database failed ", e);
+        } finally {
+            session.close();
+        }
+
     }
 
     public void modifyUser(long id, UserProfile user) {
-        Transaction transaction = session.beginTransaction();
-        UserProfile oldUser = session.load(UserProfile.class, id);
-        oldUser.setPassword(user.getPassword());
-        oldUser.setEmail(user.getEmail());
-        transaction.commit();
+        try {
+            session.getTransaction().begin();
+            UserProfile oldUser = session.load(UserProfile.class, id);
+            oldUser.setPassword(user.getPassword());
+            oldUser.setEmail(user.getEmail());
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Database failed ", e);
+        } finally {
+            session.close();
+        }
     }
 
     public void deleteUser(long id) {
-        Transaction transaction = session.beginTransaction();
-        UserProfile user = session.load(UserProfile.class, id);
-        session.delete(user);
-        transaction.commit();
+        try {
+            UserProfile user = session.load(UserProfile.class, id);
+            session.delete(user);
+        } catch (Exception e) {
+            logger.error("Database failed ", e);
+        } finally {
+            session.close();
+        }
+
     }
 }
